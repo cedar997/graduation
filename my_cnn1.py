91bdf9aab2aaa6a0477bef41b0319ec463c5a36a
cedar997 <527474091@qq.com>
2019-03-14 10:41:10 +0800

    对onehot编码进行调整,对序列空白部分进行优化"

--- my_cnn1.py
+++ my_cnn1.py
@@ -12,26 +12,37 @@ MODEL_PATH='saved_model.h5'
 def get_pssm(path):
     ds=np.load(path).item()
     pssm=np.array(ds['pssm'])
+    del ds
     num=len(pssm)
-    ret=np.zeros((num,SEQ_SIZE_MAX,20))    #29=氨基酸数21+二级结构数8 序列最多为759个氨基酸
-    #ret[ : , : , 0 : 20 ]=pssm[ : , : , 0 : 20 ]   #载入pssm
+    ret=np.zeros((num,SEQ_SIZE_MAX,21))    #29=氨基酸数21+二级结构数8 序列最多为759个氨基酸
     for i in range(num):
-        for j in range(len(pssm[i])):
-            ret[i,j,0:20]=pssm[i][j]
+        for j in range(SEQ_SIZE_MAX):
+            if j<len(pssm[i]):
+                ret[i,j,0:20]=pssm[i][j]
+            else:
+                ret[i,j,20]=1
+
     return ret
-def get_dssp(path):
+def get_dssp_raw(path):
     ds=np.load(path).item()
     dssp=np.array(ds['dssp'])
+    del ds
+    return dssp
+def get_dssp(path):
+    ds=np.load(path).item()
+    dssp=ds['dssp']
+    del ds
     num=len(dssp)
-    ret=np.zeros((num,SEQ_SIZE_MAX,3))
-    for i in range(len(dssp)):
-            for j in range(len(dssp[i])):
-                if dssp[i][j]=='E':
-                    ret[i,j,0]=1
-                elif dssp[i][j]=='H':
-                    ret[i,j,1]=1
-                else:
-                    ret[i,j,2]=1
+    print(num)
+    ret=np.zeros((num,SEQ_SIZE_MAX,4))
+    onehot_dict={'E':0,'H':1,'-':2,' ':3}
+    for i in range(num):
+            dssp[i]=dssp[i]+' '*(SEQ_SIZE_MAX-len(dssp[i]))
+            # print(dssp[i])
+            # print(i,len(dssp[i]))
+            for j in range(759):
+                k=onehot_dict[dssp[i][j]]
+                ret[i,j,k]=1
         #29=氨基酸数21+二级结构数8 序列最多为759个氨基酸
     return ret
 #np.random.seed(100)
@@ -41,10 +52,10 @@ def dssp_trans(dssp=None,onehot=None):
         for i in range(len(onehot)):
             s=''
             for j in range(len(onehot[i])):
-                if np.sum(onehot[i,j])==0:
+                if onehot[i,j][3]==1:
                     break
                 k=np.argmax(onehot[i,j])
-                s+=['E','H','-'][k]
+                s+=['E','H','-',' '][k]
             ret.append(s)
     return ret
 def Q3_accuracy(real, pred):
@@ -52,10 +63,10 @@ def Q3_accuracy(real, pred):
     correct = 0
     for i in range(real.shape[0]):  # per element in the batch
         for j in range(real.shape[1]): # per aminoacid residue
-            if np.sum(real[i, j, :]) == 0:  #  real[i, j, dataset.num_classes - 1] > 0 # if it is padding
+            if real[i, j, 3] == 1:  #  real[i, j, dataset.num_classes - 1] > 0 # if it is padding
                 total = total - 1
             else:
-                if real[i, j, np.argmax(pred[i, j, :])] > 0:
+                if real[i, j, np.argmax(pred[i, j, 0:3])] > 0:
                     correct = correct + 1
 
     return correct / total
@@ -81,11 +92,11 @@ def plot_history(history):
     
 def model():
     m = Sequential ()
-    m.add( Conv1D (128,11,padding='same',activation='relu',input_shape=( 760 ,20 ) ) )
+    m.add( Conv1D (128,11,padding='same',activation='relu',input_shape=( 760 ,21 ) ) )
     m.add(Dropout(0.3))
     m.add(Conv1D(64, 11, padding='same', activation='relu'))
     m.add(Dropout(0.3))
-    m.add(Conv1D(3, 11, padding='same', activation='softmax'))
+    m.add(Conv1D(4, 11, padding='same', activation='softmax'))
     opt = optimizers.Adam(lr=0.0005)
     m.compile(optimizer=opt,
                 loss='categorical_crossentropy',
@@ -97,13 +108,17 @@ def test_saved():
     m=load_model(MODEL_PATH)
     predictions = m.predict(X_test)
     print("\n\nQ3 accuracy: " + str(Q3_accuracy(Y_test, predictions)) + "\n\n")
+    p=dssp_trans(onehot=predictions)
+    y=get_dssp_raw('test.npy')
+    print(p[1])
+    print(y[1])
 def test():
     X_train=get_pssm('train.npy')
     Y_train=get_dssp('train.npy')
     X_test=get_pssm('test.npy')
     Y_test=get_dssp('test.npy')
     m=model()
-    history=m.fit( X_train,Y_train,epochs=100,batch_size=64,validation_split=0.1)
+    history=m.fit( X_train,Y_train,epochs=50,batch_size=64,validation_split=0.1)
     predictions = m.predict(X_test)
     print("\n\nQ3 accuracy: " + str(Q3_accuracy(Y_test, predictions)) + "\n\n")
     if MODEL_SAVE==True:
@@ -112,4 +127,5 @@ def test():
         plot_history(history)
 
 if __name__ == "__main__":
-    test()
+    # test()
+    test_saved()
